f1765f3f6f23b41040e73bdb9ddf6b68f9e97305
Glaesses3927 <42023384+Glaesses3927@users.noreply.github.com>
2024-09-05 16:09:06 +0900

    PWA化

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
 import { Auth0Provider } from '@auth0/auth0-react';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -27,3 +28,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
